9975255cb8b821f994670d6869e1778ee977243c
Salvador Viramontes <5307682-SalvadorViramontes@users.noreply.gitlab.com>
2020-08-05 21:26:30 -0500

    Initial Commit

---
+++ src/router/index.ts
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+  const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue'),
+    meta: {
+      title: 'About',
+    }
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((toRoute, _fromRoute, next) => {
+  window.document.title = toRoute.meta && toRoute.meta.title ? toRoute.meta.title : 'QuizApp';
+  next();
+})
+
+export default router
---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an app to test your knowledge</h1>
+  </div>
+</template>
